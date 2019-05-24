7c29fd5a52030fc212c2ab1a35a48b26d65103f0
蔡泽欣 <qq.co>
2019-05-24 17:58:40 +0800

    初始提交

--- vue-admin/src/assets/js/common.js
+++
@@ -1,5 +0,0 @@
-export function authValid () {
-  let logininfo = sessionStorage.getItem('logininfo') ? JSON.parse(sessionStorage.getItem('logininfo')) : {}
-  let auth = logininfo.auth === 'superAdmin' ? 'superAdmin' : logininfo.auth === 'admin' ? 'admin' : 'user'
-  return auth
-}
